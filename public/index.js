6101c30c4a2aa457774e8933da0d89be964feddd
hnini <n_hakim_92i@hotmail.fr>
2016-01-14 20:09:53 +0100

    Final commit, exo 6, apply changes on rental

--- public/index.js
+++ public/index.js
@@ -22,9 +22,20 @@ function GetCar(rentalCarId, cars) //This function allows to get the associated 
 	}
 }
 
-function SetPercentage(NumberDays)
+function GetRental(rentalId, rentals) //This function allows to get the associated rental, given a rental id (it is used for the rental modification)
 {
-var percent = 1; // compute of the percentage for the reduction
+	for(var rental of rentals) //here we get all the rental in the array 'rentals'
+	{ 
+		if (rentalId == rental.id) //then compare the both id (the given id in the parameters of the function, and the id of the rental in the array)
+		{
+			return rental;
+		}
+	}
+}
+
+function SetPercentage(NumberDays)// compute of the percentage for the reduction
+{
+var percent = 1; 
 		
 			if (NumberDays >1 && NumberDays <= 4) //if the number of days is lower tha 4
 			percent = 0.9;
@@ -38,7 +49,7 @@ var percent = 1; // compute of the percentage for the reduction
 			return percent;
 }
 
-function SetActorAmount (rentalId, who, amount, actors) //this function is tricky becaus 'actor' is an array of objects which contents an array of objects
+function SetActorAmount (rentalId, who, amount, actors) //this function is tricky because 'actor' is an array of objects which contents an array of objects
 {
 	for (var theActor of actors) //for each instance of the array actors
 	{
@@ -90,7 +101,19 @@ function Price(rentals, actors) //Compute the price of the rental.
 	}
 }
 
+function ChangeRental(rentals, Mrentals) //apply the chegements on the rental array, Mrental is for rentalModification
+{
+	 for (var Mrental of Mrentals) //first get the modified rentals
+		{
+			var rental = GetRental(Mrental.rentalId, rentals); //then get the unmodified rentals to apply them the changes
+			
+			for (var properties in Mrental)
+			{
+				rental[properties] = Mrental[properties]; //apply changes
+			}
+		}
 
+}
 var cars = [{
   'id': 'p306',
   'vehicule': 'peugeot 306',
@@ -255,6 +278,13 @@ var rentalModifications = [{
 }];
 
 Price(rentals,actors);
+
+var changeRental = confirm("Press ok to change the rentals and compute the new price");
+if (changeRental == true) {
+    ChangeRental(rentals, rentalModifications);
+	Price(rentals,actors);
+} 
+
 console.log(cars);
 console.log(rentals);
 console.log(actors);
